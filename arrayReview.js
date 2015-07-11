0ddfa3d824879b412e5cecfd00997d30a9803d41
Kristi Madsen <kristimadsen@gmail.com>
2015-07-11 12:54:58 -0600

    ouchy

--- arrayReview.js
+++ arrayReview.js
@@ -2,9 +2,11 @@ var threeItems = [1,2,3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
+var last = function (arr){
+	return arr[arr.length - 1];
+};
 
-
+console.log(last(threeItems));
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -13,34 +15,70 @@ var threeItems = [1,2,3];
 //Loop through evenArray removing all values that aren't even 
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
+var evenGone = function (arr) {
+	for (var i = arr.length; i >= 0; i--) {
+		if (arr[i] % 2 === 0) {
+			arr.splice(i, 1); 
+		}
+	}
+	return arr;
+};
+
+console.log(evenGone(evenArray));
+
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. 
+//Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. 
+//If it is, alert true, if it's not, alert false
+
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+var numHere = function(arr) {
+	var randomNum = getRandomArbitrary();
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] === randomNum) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+
+console.log(numHere(randomArray));
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
+//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. 
+//When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
+
 var first = [1,2,3,4,5];
 var second;
 
-  //code here
+var copy = function (arr) {
+    var holdingArr = [];
+	for (var i = 0; i <= arr.length; i++) {
+		holdingArr.push(i);
+	}
+    return holdingArr;
+};
 
-alert(first) //[1,2,3,4,5];
-alert(second) //[1,2,3,4,5,6,7];
+second = (copy(first));
+second.push(6, 7);
+
+alert(first); //[1,2,3,4,5];
+alert(second); //[1,2,3,4,5,6,7];
 
 
 
@@ -49,9 +87,22 @@ alert(second) //[1,2,3,4,5,6,7];
 
 
 //write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
-var sentence = "Dev Mountain is the best"
+var sentence = "Dev Mountain is the best";
 
-  //code here
+var longest = function(str) {
+	var strArr = str.split(" ");
+	var longestWordLength = 0;
+	var longestWord;
+	for (var i = 0; i < strArr.length; i++) {
+		if (longestWordLength < strArr[i].length {
+		longestWordLength = strArr[i].length;
+		longestWord = strArr[i];
+		}
+	}
+	return longestWord;	
+};
+
+console.log(longest(sentence));
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -62,13 +113,33 @@ var sentence = "Dev Mountain is the best"
 var myPoem = 'What is a jQuery but a misunderstood object?'
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
-  //code here
+var capitalize = function(str) {
+	var strArr = str.split(" ");
+	for (var i = 0; i < strArr.length; i++) {
+		var upperChar = strArr[i].charAt(0).toUpperCase();
+		strArr[i] = upperChar + strArr[i].substr(1);
+		}
+	return strArr.join(" ");
+};
 
+console.log(capitalize(myPoem));
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
+//Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
 
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
-//Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+
+var vowelCounter = function (str) {
+	var count = 0;
+	var strArr = str.toLowerCase().split("");
+	for (var i = 0; i < strArr.length; i++) {
+		if (strArr[i] === "a" || strArr[i] === "e" || strArr[i] === "i" || strArr[i] === "o" || strArr[i] === "u" ) {
+			count++;
+		} 
+	}
+	return count;
+};
+
+console.log(vowelCounter(theOdyssey));
